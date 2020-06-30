dfe27973e5d11fa0ad4939f611438652136be64d
Kristijan Jovanovski <kristijan.j.mail@gmail.com>
2020-06-30 22:09:11 +0200

    memory data

--- c1/handlers/index.js
+++ c1/handlers/index.js
@@ -1,11 +1,24 @@
+const students = [
+  {fname: 'Pero', lname: 'Perovski', gpa: 9.2},
+  {fname: 'Stanko', lname: 'Stankovski', gpa: 7.0},
+  {fname: 'Janko', lname: 'Jankovski', gpa: 6.1},
+]
+
 const getAllStudents = (req, res) => {
-  res.status(200).send('ok');
+  return res.status(200).send(students);
 }
 const getSingleStudent = (req, res) => {
-  res.status(200).send('ok');
+  if (students[req.params.id] !== undefined) {
+    return res.status(200).send(students[req.params.id]);
+  }
+  return res.status(404).send('Not Found');
 }
 const createStudent = (req, res) => {
-  res.status(200).send('ok');
+  if (req.body){
+    students.push(req.body);
+    return res.status(201).send('ok');
+  }
+  res.status(400).send('Bad Request');
 }
 const removeStudent = (req, res) => {
   res.status(200).send('ok');
