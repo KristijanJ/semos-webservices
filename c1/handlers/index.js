49a40432ceb64828ba2ec0bafe6851bebc42801b
Kristijan Jovanovski <kristijan.j.mail@gmail.com>
2020-07-01 17:04:58 +0200

    remove, update, patch student

--- c1/handlers/index.js
+++ c1/handlers/index.js
@@ -1,34 +1,65 @@
 const students = [
-  {fname: 'Pero', lname: 'Perovski', gpa: 9.2},
-  {fname: 'Stanko', lname: 'Stankovski', gpa: 7.0},
-  {fname: 'Janko', lname: 'Jankovski', gpa: 6.1},
-]
+  { fname: "Pero", lname: "Perovski", gpa: 9.2 },
+  { fname: "Stanko", lname: "Stankovski", gpa: 7.0 },
+  { fname: "Janko", lname: "Jankovski", gpa: 6.1 },
+];
 
 const getAllStudents = (req, res) => {
   return res.status(200).send(students);
-}
+};
+
 const getSingleStudent = (req, res) => {
   if (students[req.params.id] !== undefined) {
     return res.status(200).send(students[req.params.id]);
   }
-  return res.status(404).send('Not Found');
-}
+  return res.status(404).send("Not Found");
+};
+
 const createStudent = (req, res) => {
-  if (req.body){
+  if (req.body) {
     students.push(req.body);
-    return res.status(201).send('ok');
+    return res.status(201).send("ok");
   }
-  res.status(400).send('Bad Request');
-}
+  res.status(400).send("Bad Request");
+};
+
 const removeStudent = (req, res) => {
-  res.status(200).send('ok');
-}
+  if (students[req.params.id] !== undefined) {
+    const student = students[req.params.id];
+    students.splice(req.params.id, 1);
+    return res.status(200).send({
+      message: "Student deleted",
+      student: student,
+    });
+  }
+  return res.status(404).send("Not Found");
+};
+
 const updateStudent = (req, res) => {
-  res.status(200).send('ok');
-}
+  if (students[req.params.id] !== undefined) {
+    students[req.params.id] = req.body;
+    return res.status(200).send({
+      message: "Student updated",
+      student: students[req.params.id],
+    });
+  }
+  return res.status(404).send("Not Found");
+};
+
 const patchStudent = (req, res) => {
-  res.status(200).send('ok');
-}
+  if (students[req.params.id] !== undefined) {
+    students[req.params.id] = {
+      'fname': req.body.fname ? req.body.fname : students[req.params.id].fname,
+      'lname': req.body.lname ? req.body.lname : students[req.params.id].lname,
+      'gpa': req.body.gpa ? req.body.gpa : students[req.params.id].gpa
+    }
+    return res.status(200).send({
+      message: "Student patched",
+      student: students[req.params.id],
+    });
+  }
+  return res.status(404).send("Not Found");
+};
 
 module.exports = {
   getAllStudents,
@@ -36,5 +67,5 @@ module.exports = {
   createStudent,
   removeStudent,
   updateStudent,
-  patchStudent
-}
+  patchStudent,
+};
