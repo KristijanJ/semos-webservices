50318b3b1dab2f54f119cc2da082b3edb4a30422
Kristijan Jovanovski <kristijan.j.mail@gmail.com>
2020-07-02 21:31:12 +0200

    fs read, update, wrie

---
+++ c2/index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+/* CREATE A NEW FILE AND WRITE TO IT */
+// fs.writeFile('test.txt', 'Hello World', (err) => {
+//   if (err) {
+//     return console.error(err);
+//   }
+//   console.log('Write successful');
+// });
+
+/* EDIT OR CREATE A FILE AND WRITE TO IT */
+// fs.appendFile('test.txt', 'Hello Worldz', (err) => {
+//   if (err) {
+//     return console.error(err);
+//   }
+//   console.log('Write successful');
+// });
+
+/* READ A FILE */
+// fs.readFile('test.txt', 'utf8', (err, data) => {
+//   if (err) {
+//     return console.error(err);
+//   }
+//   console.log(data);
+// });
+
+/* DELETE A FILE */
+// fs.unlink('test.txt', (err) => {
+//   if (err) {
+//     return console.error(err);
+//   }
+//   console.log('File deleted');
+// });
+
