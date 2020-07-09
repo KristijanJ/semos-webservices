1b91840d7e8b2a6293e98bdbe4a5cf6a1e7d1d89
Kristijan Jovanovski <kristijan.j.mail@gmail.com>
2020-07-09 22:04:20 +0200

    c4, students working with mongo

--- c3/handlers/students.js
+++ c3/handlers/students.js
@@ -1,25 +1,71 @@
+const file = require('../pkg/file');
+const students = require('../pkg/students');
+
+
 const getAll = (req, res) => {
-  return res.status(200).send('ok');
+  students.getAll()
+    .then(data => {
+      res.status(200).send(data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send('internal server error');
+    });
 };
 
 const getSingle = (req, res) => {
-  return res.status(200).send('ok');
+  students.getOne(req.params.id)
+    .then(data => {
+      res.status(200).send(data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send('internal server error');
+    });
 };
 
 const create = (req, res) => {
-  return res.status(200).send('ok');
+  students.create(req.body)
+    .then(() => {
+      res.status(201).send('created');
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send('internal server error');
+    });
 };
 
 const remove = (req, res) => {
-  return res.status(200).send('ok');
+  students.remove(req.params.id)
+    .then(() => {
+      res.status(204).send('no content');
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send('internal server error');
+    });
 };
 
 const update = (req, res) => {
-  return res.status(200).send('ok');
+  students.update(req.params.id, req.body)
+    .then(() => {
+      res.status(204).send('no content');
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send('internal server error');
+    });
 };
 
 const patch = (req, res) => {
-  return res.status(200).send('ok');
+  students.patch(req.params.id, req.body)
+    .then(() => {
+      res.status(204).send('no content');
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send('internal server error');
+    });
 };
 
 module.exports = {
