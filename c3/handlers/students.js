fe9fdf8ac0d05b353b9cc1668543d4486b9eb391
Kristijan Jovanovski <kristijan.j.mail@gmail.com>
2020-07-16 18:41:53 +0200

    classes, validation

--- c3/handlers/students.js
+++ c3/handlers/students.js
@@ -1,70 +1,84 @@
-const file = require('../pkg/file');
-const students = require('../pkg/students');
-
+const students = require("../pkg/students");
+const studentValidator = require("../pkg/students/validation");
 
 const getAll = (req, res) => {
   students.getAll()
-    .then(data => {
+    .then((data) => {
       res.status(200).send(data);
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
-      res.status(500).send('internal server error');
+      res.status(500).send("internal server error");
     });
 };
 
 const getSingle = (req, res) => {
   students.getOne(req.params.id)
-    .then(data => {
+    .then((data) => {
       res.status(200).send(data);
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
-      res.status(500).send('internal server error');
+      res.status(500).send("internal server error");
     });
 };
 
 const create = (req, res) => {
-  students.create(req.body)
+  studentValidator.validate(req.body)
+    .then((matches) => {
+      if (!matches) {
+        throw "Bad request";
+      }
+      return students.create(req.body);
+    })
     .then(() => {
-      res.status(201).send('created');
+      res.status(201).send("created");
     })
     .catch((err) => {
       console.log(err);
-      res.status(500).send('internal server error');
+      res.status(500).send("internal server error");
     });
 };
+
+// const createCB = (req, res) => {
+//     students.create(req.body, (err) => {
+//         if(err) {
+//             return res.status(500).send('internal server error');
+//         }
+//         return res.status(500).send('created');
+//     });
+// };
 
 const remove = (req, res) => {
   students.remove(req.params.id)
     .then(() => {
-      res.status(204).send('no content');
+      res.status(204).send("no content");
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
-      res.status(500).send('internal server error');
+      res.status(500).send("internal server error");
     });
 };
 
 const update = (req, res) => {
   students.update(req.params.id, req.body)
     .then(() => {
-      res.status(204).send('no content');
+      res.status(204).send("no content");
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
-      res.status(500).send('internal server error');
+      res.status(500).send("internal server error");
     });
 };
 
 const patch = (req, res) => {
-  students.patch(req.params.id, req.body)
+  students.update(req.params.id, req.body)
     .then(() => {
-      res.status(204).send('no content');
+      res.status(204).send("no content");
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
-      res.status(500).send('internal server error');
+      res.status(500).send("internal server error");
     });
 };
 
@@ -74,5 +88,5 @@ module.exports = {
   create,
   remove,
   update,
-  patch
+  patch,
 };
